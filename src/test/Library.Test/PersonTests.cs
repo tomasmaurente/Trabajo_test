ec92acc9be5a2377e3460ed90eaf0c7c3d3d7b3c
tomasmaurente <tommaurente@gmail.com>
2021-05-09 15:39:37 -0300

    Se agregaron 2 test y se corrijiron los comentarios

--- src/test/Library.Test/PersonTests.cs
+++ src/test/Library.Test/PersonTests.cs
@@ -5,36 +5,58 @@ namespace Tests
 {
     public class Tests
     {
-        
         private Person persona;
+
         [SetUp]
         public void Setup()
         {
-            //Patron triple A
-            //Arrange
-            this.persona = new Person("","");
+            // Patron triple A.
+            // Arrange.
+            this.persona = new Person("", "");
         }
+
         [Test]
-        //Se testea que sucede cuando al contructor se le pasan strings vacios "" 
+        // Se testea que sucede cuando al contructor se le pasan strings vacios "". 
         public void ConstructorNombreVacio()
         {
-            Assert.AreEqual(this.persona.Name,null);
+            Assert.AreEqual(this.persona.Name, null);
         }
+
+        [Test]
         public void ConstructorIdVacio()
         {
-            Assert.AreEqual(this.persona.ID,null);
+            Assert.AreEqual(this.persona.ID, null);
         }
+
         [Test]
-        //Se testea la asignacion de null
+        // Se testea la asignacion de null.
         public void NameNull()
         {
-            //Arragne
-
-            //Act: llamar a lo que queremos probar (metodo, propiedad)
+            // Act: llamar a lo que queremos probar (metodo, propiedad).
             this.persona.Name = null;
             //Assert
-            Assert.AreEqual(persona.Name,"");
+            Assert.AreEqual(null, persona.Name);
         }
 
+        [Test]
+        // Se testea que la cedula sea "null" si se ingresa con un formato incorrecto.
+        // Encontramos que el programa no tiene en cuenta el formato de el Id.
+        public void IdFormatoIncorrecto()
+        {
+            // Act.
+            this.persona.ID = "52115803";
+            // Assert.
+            Assert.AreEqual(null, persona.ID);
+        }
+
+        [Test]
+        // Se testea que la cedula sea "null" si se ingresa con numeros de más.
+        public void IdNumeroInCorrecto()
+        {
+            // Act.
+            this.persona.ID = "52115856803";
+            // Assert.
+            Assert.AreEqual(null, persona.ID);
+        }
     }
 }
